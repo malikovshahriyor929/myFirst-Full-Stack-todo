e1ea835ae2e43199a34fe370ddabb7602183e860
malikovshahriyor <malikovshahriyor929@gmail.com>
2025-06-14 20:34:40 +0500

    add import

--- src/app/backend/models/todo.model.js
+++ src/app/backend/models/todo.model.js
@@ -1,10 +1,23 @@
-import { Schema, model } from "mongoose";
+// import { Schema, model } from "mongoose";
+
+// const TodoSchema = new Schema(
+//   {
+//     text: { type: String, required: true },
+//   },
+//   { timestamps: true }
+// );
+
+// export default model("Todo", TodoSchema);
+
+const { Schema, model } = require("mongoose");
 
 const TodoSchema = new Schema(
   {
-    text: { type: String, required: true },
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    picture: { type: String },
   },
   { timestamps: true }
 );
 
-export default model("Todo", TodoSchema);
+export default model("Post", TodoSchema);
